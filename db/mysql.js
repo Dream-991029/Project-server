48308cf49f9c601f26a2808852f259263ac06c77
Dream <981846339@qq.com>
2021-11-19 02:45:40 +0800

    修改数据库主机、密码

--- db/mysql.js
+++ db/mysql.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql');
 // 创建数据库连接对象
 const db = mysql.createPool({
-  host: '192.168.3.82',
+  host: '192.168.10.7',
   port: 3306,
   user: 'root',
-  password: 'twd@root$9921',
+  password: 'root',
   database: 'project_01'
 });
 // 导出db对象
--- router-handler/userinfo.js
+++ router-handler/userinfo.js
@@ -46,10 +46,9 @@ module.exports.getUserInfo = (req, res) => {
   if (valueErr) return res.ck(valueErr)
   // 获取用户信息sql
   const sqlGetUserInfo = '(SELECT info.user_id,info.user_name,type.user_type,info.phone_number,gen.sex,info.status,info.user_name as create_by,info.create_time,info.update_by,info.update_time,info.remark FROM sys_user info,sys_user info2,sys_user_sex gen,sys_user_type type WHERE info.sex=gen.sex_id AND info.user_type=type.user_type_id AND info2.create_by=info.user_id AND info.del_flag=0 AND info.user_name=? AND info2.update_by IS null) UNION (SELECT temp.user_id,temp.user_name,temp.user_type,temp.phone_number,temp.sex,temp.status,temp.create_by,temp.create_time,temp2.user_name as update_by,temp.update_time,temp.remark FROM sys_user as temp2,(SELECT info.user_id,info.user_name,type.user_type,info.phone_number,gen.sex,info.status,info.user_name as create_by,info.create_time,info.update_by,info.update_time,info.remark FROM sys_user info,sys_user info2,sys_user_sex gen,sys_user_type type WHERE info.sex=gen.sex_id AND info.user_type=type.user_type_id AND info2.create_by=info.user_id AND info.del_flag=0 AND info.user_name=?) as temp WHERE temp.update_by=temp2.user_id AND temp2.del_flag=0)'
-  // sqlGetUserInfoCount = 'SELECT COUNT(*) AS total FROM sys_user'
   db.query(sqlGetUserInfo, [data.user_name, data.user_name], (err, results) => {
     if (err) return res.ck(err)
-    if (results.length === 0) return res.ck('查询失败, 此用户不存在!')
+    if (results.length === 0) return res.ck('查询失败, 此帐号不存在!')
     let obj = results[0]
     return res.ck(obj, '查询成功!', 0)
   })
