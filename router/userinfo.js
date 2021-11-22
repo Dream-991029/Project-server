6a6e4517a44a4088aca8fcdb439cdfe264d0c278
Dream-991029 <981846339@qq.com>
2021-11-22 18:10:59 +0800

    修改注册获取用户接口sql语句

--- router/userinfo.js
+++ router/userinfo.js
@@ -7,7 +7,7 @@ const userInfoHandler = require("../router-handler/userinfo");
 // 创建获取用户信息列表路由
 router.get('', userInfoHandler.getUserInfoList);
 // 创建获取单个用户信息路由
-router.get('/:user_name', userInfoHandler.getUserInfo)
+router.get('/info', userInfoHandler.getUserInfo)
 // 添加用户路由
 router.post('/adduser', userInfoHandler.addUser)
 
